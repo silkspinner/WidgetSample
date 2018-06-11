e15c7325c50334baec2a1245867d1311b12e822f
Ron Nims <rleenims@gmail.com>
2018-06-11 16:34:52 -0700

    creates initial version of App Widget test bed

---
+++ app/src/main/java/com/nimsdev/widgetsample/ExampleAppWidgetProvider.java
@@ -0,0 +1,26 @@
+package com.nimsdev.widgetsample;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class ExampleAppWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        //super.onUpdate(context, appWidgetManager, appWidgetIds);
+        for (int appWidgetId : appWidgetIds) {
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_widget);
+            views.setOnClickPendingIntent(R.id.example_widget_button, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
+
+
